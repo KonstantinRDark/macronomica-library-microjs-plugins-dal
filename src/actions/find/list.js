12411950e0febf6a9eac6ed955cab70cc1922cae
Maxim Konkin <mk@macronomica.com>
2016-11-08 19:19:21 +0300

    Исправил баг в list

--- src/actions/find/list.js
+++ src/actions/find/list.js
@@ -65,7 +65,7 @@ export function buildFindList(middleware, schema, criteria = {}, options = {}) {
           if (manyLinks.length > 0) {
             result = result.map(item => {
               manyLinks.forEach(name => {
-                if (name in item) {
+                if (name in item && !!item[ name ]) {
                   item[ name ] = item[ name ].split(',');
                 }
               });
